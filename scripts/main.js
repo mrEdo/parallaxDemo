0dd5e7fb4666dd8a601959cfc89ac76858be966a
mrEdo <admin@Lab-2-Instructor.local>
2017-02-13 19:08:34 -0600

    Mouse Controlled Parallax Added with jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 // My Scripts
+
+// $(document).scroll(function(){
+// 	console.log( $(document).scrollTop() );
+
+// 	$("#frontclouds").css("left", String($(document).scrollTop() - 70) + "px" );
+// 	$("#middleclouds").css("left", String($(document).scrollTop() * .5) + "px" );
+// 	$("#backclouds").css("left", String($(document).scrollTop() * .2) + "px" );
+// });
+
+$(document).scroll(function(){
+	console.log( $(document).scrollTop() );
+
+	$("#frontclouds").css("left", String($(document).scrollTop() - 70) + "px" );
+	$("#middleclouds").css("left", String($(document).scrollTop() * .5) + "px" );
+	$("#backclouds").css("left", String($(document).scrollTop() * .2) + "px" );
+});
+
+$(document).mousemove(function(p){
+	console.log(p.pageX + ", " + p.pageY);
+	if(p.pageX > 200 && p.pageX < 800 ){
+		$("#frontclouds").css("left", String(p.pageX - 800) + "px" );
+		$("#middleclouds").css("left", String(p.pageX * .5 - 400) + "px" );
+		$("#text").css("left", String(p.pageX * .35) + "px" );
+		$("#backclouds").css("left", String(p.pageX * .2 - 200) + "px" );
+	}
+	
+});
