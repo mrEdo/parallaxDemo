196c788c4d7972d738989952d11b7684bfe22d4d
mrEdo <admin@Lab-2-Instructor.local>
2017-02-13 22:13:58 -0600

    Spritesheet Character Animation Added

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@
 // });
 
 $(document).scroll(function(){
-	console.log( $(document).scrollTop() );
+	//console.log( $(document).scrollTop() );
 
 	$("#frontclouds").css("left", String($(document).scrollTop() - 70) + "px" );
 	$("#middleclouds").css("left", String($(document).scrollTop() * .5) + "px" );
@@ -17,7 +17,7 @@ $(document).scroll(function(){
 });
 
 $(document).mousemove(function(p){
-	console.log(p.pageX + ", " + p.pageY);
+	//console.log(p.pageX + ", " + p.pageY);
 	if(p.pageX > 200 && p.pageX < 800 ){
 		$("#frontclouds").css("left", String(p.pageX - 800) + "px" );
 		$("#middleclouds").css("left", String(p.pageX * .5 - 400) + "px" );
@@ -26,3 +26,79 @@ $(document).mousemove(function(p){
 	}
 	
 });
+
+
+
+
+
+
+
+
+$(document).scroll(function(){
+	$("#mountains").css("left", String($(document).scrollTop()*(-1) ) + "px" );
+	$("#clouds").css("left", String($(document).scrollTop()*(-1)/3 ) + "px" );
+	$("#character").css("left", String($(document).scrollTop()/2 ) + "px" );
+	console.log( $(document).scrollTop() );
+	
+	$("#row1").css("right", String($(document).scrollTop() - 1000) + "px");
+	$(".pics").css("transform","rotate(" + $(document).scrollTop() + "deg)");
+
+	if( $(document).scrollTop() > 0 ){
+		$("#row2").css("left", String($(document).scrollTop() - 1000) + "px");
+	}
+	
+});
+
+var animationCount = 0;
+var guyMoving = false;
+var moveGuyTimer;
+
+function showNum(){
+	animationCount++;
+	if(animationCount > 4){
+		animationCount = 0;
+		clearInterval(moveGuyTimer);
+		guyMoving = false;
+	}
+	if(animationCount == 1){
+
+		document.getElementById("character").style.backgroundPosition = "0px 0px";
+		//$("#charcter").css("background-color","red");
+	} else if(animationCount == 2){
+		//$("#charcter").css("background-position","206px 0px");
+		document.getElementById("character").style.backgroundPosition = "206px 0px";
+	} else if(animationCount == 3){
+		//$("#charcter").css("background-position","412px 0px");
+		document.getElementById("character").style.backgroundPosition = "412px 0px";
+	} else if(animationCount == 4){
+		document.getElementById("character").style.backgroundPosition = "618px 0px";
+		//$("#charcter").css("background-position","618px 0px");
+	}
+	//console.log(animationCount);
+}
+
+$(document).scroll(function(){
+	if(guyMoving == false){
+		moveGuyTimer = setInterval(showNum, 250);
+		guyMoving = true;
+	}
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
